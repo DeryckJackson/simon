8e0ca9d0031ced4106971201f8e79a63f0ed23a3
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-22 16:17:56 -0700

    add colors class in simon
    co-authored-by: Frederick Ernest <frederick.ernest@gmail.com>
    Co-authored-by: Matthew Craig <mgcraig78@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
+import { Colors } from './simon.js';
+import $ from 'jquery';
 import './styles.css';
+
+
+$(document).ready(function() {
+  let currentColors = new Colors();
+  $("#color").submit(function(event) {
+    event.preventDefault();
+    currentColors.colors.push(currentColors.getColor());
+    $(".output").text(currentColors.colors[0] + " ");
+    console.log(currentColors);
+  });
+
+  $("#red").submit(function(event) {
+    event.preventDefault();
+    $(".output").text("Sorry, you guessed wrong.");
+  })
+});
--- src/simon.js
+++ src/simon.js
@@ -1,3 +1,27 @@
-function getColor() {
-  colors = ["red, blue, green"]
+export class Colors {
+  constructor() {
+    this.colors = [];
+    this.colorIndex = 0;
+  }
+
+  checkColor(userColor) {
+    if (this.colors[this.colorIndex] === userColor) {
+      this.colorIndex++;
+      return true;
+    } else {
+      this.colors = [];
+      this.colorIndex = 0;
+      return false;
+    }
+  }
+
+  getColor() {
+    const colors = ["red", "blue", "green"];
+    
+    return colors[getRandomInt(0, 2)];
+  }
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
